2449dcb1fdb61c2bb80abd228eb6bac615c1384c
SongHao <mail@songhao.xyz>
2016-06-25 22:32:31 +0800

    delete irrelevant front-end code
    delete irrelevant front-end code

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,18 +1,10 @@
 'use strict';
 
-angular.module('adWebHW')
+angular.module('SELab3')
     .constant("baseURL", "http://localhost:3000/")
-    .service('bookFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
-        this.getBooks = function () {
-            var data = $resource(baseURL + "books/:id", null, {'update': {method: 'PUT', responseType: 'json'}});
-            console.log(data);
-            return data;
-        };
-    }])
-
-    .service('feedbackFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
-        this.getFeedbacks = function () {
-            var data =  $resource(baseURL + "feedback", null, {'update': {method: 'PUT'}});
+    .service('meetingFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+        this.getMeeting = function () {
+            var data = $resource(baseURL + "meeting/:id", null, {'update': {method: 'PUT', responseType: 'json'}});
             console.log(data);
             return data;
         };
