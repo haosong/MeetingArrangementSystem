58481165dcfa4832a76c2632114cbc25857ef7f7
Song Hao <songhao1119@gmail.com>
2016-06-28 14:06:52 +0800

    sync font-end code
    sync font-end code with src/main/webapp

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -17,77 +17,12 @@ angular.module('SELab3')
         $scope.sponsor = '';
         $scope.choices = [{id: '1', 'name': '', 'attend': false}];
         $scope.content = '';
+        $scope.meetingRadio = {value: ''};
         $scope.isRecommend = false;
-        $scope.successResponse = {
-            "meeting": {
-                "duration": 30,
-                "sponsor": "a",
-                "start": "2016-07-01 14:00:00.0",
-                "id": 1,
-                "title": "b",
-                "employees": ["c", "d"],
-                "roomId": 0,
-                "content": "hhh"
-            }, "status": "0"
-        };
+        $scope.isSuccess = false;
+        $scope.isNetworkError = false;
+        $scope.successResponse = '';
         $scope.failResponse = '';
-        /*{
-            "available": [{
-                "duration": 30,
-                "sponsor": "a",
-                "start": "2016-07-01 14:40:00.0",
-                "id": 0,
-                "title": "b",
-                "employees": ["b"],
-                "roomId": 0,
-                "content": "hhh"
-            }, {
-                "duration": 30,
-                "sponsor": "a",
-                "start": "2016-07-01 16:40:00.0",
-                "id": 0,
-                "title": "b",
-                "employees": ["b"],
-                "roomId": 0,
-                "content": "hhh"
-            }, {
-                "duration": 30,
-                "sponsor": "a",
-                "start": "2016-07-01 18:40:00.0",
-                "id": 0,
-                "title": "b",
-                "employees": ["b"],
-                "roomId": 0,
-                "content": "hhh"
-            }, {
-                "duration": 30,
-                "sponsor": "a",
-                "start": "2016-07-04 11:10:00.0",
-                "id": 0,
-                "title": "b",
-                "employees": ["b"],
-                "roomId": 0,
-                "content": "hhh"
-            }, {
-                "duration": 30,
-                "sponsor": "a",
-                "start": "2016-07-04 13:10:00.0",
-                "id": 0,
-                "title": "b",
-                "employees": ["b"],
-                "roomId": 0,
-                "content": "hhh"
-            }, {
-                "duration": 30,
-                "sponsor": "a",
-                "start": "2016-07-04 15:10:00.0",
-                "id": 0,
-                "title": "b",
-                "employees": ["b"],
-                "roomId": 0,
-                "content": "hhh"
-            }], "status": "-1"
-        };*/
 
         $scope.addNewChoice = function () {
             var newItemNo = $scope.choices.length + 1;
@@ -102,45 +37,97 @@ angular.module('SELab3')
         };
 
         $scope.confirm = function () {
+            $scope.isSuccess = false;
+            $scope.isRecommend = false;
+            $scope.isNetworkError = false;
             var date = moment($('#datetimepicker4').val()).format("MM/DD/YYYY HH:mm");
-            var names = "";
-            var attend = "";
+
+            var isNameDuplicate = false;
             for (var index in $scope.choices) {
-                names = names + $scope.choices[index].name + ",";
-                attend = attend + $scope.choices[index].attend + ",";
+                for (var i = parseInt(index) + 1; i < $scope.choices.length; i++) {
+                    //console.log("----");
+                    //console.log(index);
+                    //console.log(i);
+                    if ($scope.choices[index].name === $scope.choices[i].name) {
+                        console.log("duplicate!");
+                        isNameDuplicate = true;
+                    }
+                }
             }
-            console.log('subject: ' + $scope.subject);
-            console.log('date: ' + date);
-            console.log('time: ' + $scope.time);
-            console.log('sponsor: ' + $scope.sponsor);
-            console.log('names: ' + names);
-            console.log('attend: ' + attend);
-            console.log('content: ' + $scope.content);
-            $http.get('/rest/meeting/create', {
+            if (isNameDuplicate) {
+                alert("参会人员重复！");
+            } else if ($scope.time <= 0) {
+                alert("会议时长需大于零！")
+            } else {
+                var names = "";
+                var attend = "";
+                for (var index in $scope.choices) {
+                    names = names + $scope.choices[index].name + ",";
+                    attend = attend + $scope.choices[index].attend + ",";
+                }
+                console.log('subject: ' + $scope.subject);
+                console.log('date: ' + date);
+                console.log('time: ' + $scope.time);
+                console.log('sponsor: ' + $scope.sponsor);
+                console.log('names: ' + names);
+                console.log('attend: ' + attend);
+                console.log('content: ' + $scope.content);
+                $http.get('/rest/meeting/create', {
                     params: {
                         subject: $scope.subject,
                         date: date,
+                        sponsor: $scope.sponsor,
                         time: $scope.time,
                         names: names,
                         attend: attend,
                         content: $scope.content
                     }
                 })
-                .success(function (response) {
-                    console.log(response);
-                    console.log("success");
-                    //response.status = -1;
-                    if (response.status == -1) {
-                        $scope.failResponse = response;
-                        $scope.isRecommend = true;
-                    } else {
+                    .success(function (response) {
+                        console.log("success");
+                        if (response.status == -1) {
+                            // 推荐会议
+                            $scope.failResponse = response;
+                            $scope.isRecommend = true;
+                        } else {
+                            // 会议安排成功
+                            $scope.isSuccess = true;
+                        }
+                    })
+                    .error(function (response) {
+                        // 网络错误
+                        console.log("error");
+                        $scope.isNetworkError = true;
+                    });
+            }
+        };
 
-                    }
-                })
-                .error(function (data, header, config, status) {
-                    console.log("error");
-                });
+        //Choose one from all the recommended meetings.
+        $scope.select = function () {
+            var index = $scope.meetingRadio.value;
+            var selectedMeeting = $scope.failResponse.available[index];
+            //console.log($scope.failResponse.available[index]);
+            //console.log(JSON.stringify(selectedMeeting.employees));
+            //console.log(JSON.stringify(selectedMeeting.attend));
+            $http.get('/rest/meeting/create', {
+                params: {
+                    subject: selectedMeeting.title,
+                    date: selectedMeeting.start,
+                    time: selectedMeeting.duration,
+                    names: selectedMeeting.names,
+                    sponsor: selectedMeeting.sponsor,
+                    attend: selectedMeeting.attend,
+                    content: selectedMeeting.content
+                }
+            }).success(function (response) {
+                console.log("success");
+                $scope.isRecommend = false;
+                $scope.isSuccess = true;
+            }).error(function (response) {
+                // 网络错误
+                console.log("error");
+                $scope.isNetworkError = true;
+            });
         }
-
     })
 ;
